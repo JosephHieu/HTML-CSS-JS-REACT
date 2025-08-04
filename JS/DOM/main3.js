810f2385741e9788f8ec5ae6e6188b8290ba5cad
josephhieu <joseph.nguyenhoanghieu@gmail.com>
2025-08-04 10:15:30 +0700

    ClassList Properties

---
+++ JS/DOM/main3.js
@@ -0,0 +1,38 @@
+
+// Node Properties
+
+var boxElement = document.querySelector('.box2');
+
+// console.log(boxElement.style);
+
+// boxElement.style.width = '100px';
+// boxElement.style.height = '200px';
+// boxElement.style.backgroundColor = 'red';
+
+Object.assign(boxElement.style, {
+    width: '200px',
+    height: '100px',
+    backgroundColor: 'green'
+});
+
+console.log(boxElement.style.width);
+
+
+// ClassList property
+
+// add
+// contains
+// remove
+// toggle
+
+console.log(boxElement.classList.length);
+console.log(boxElement.classList.value);
+
+boxElement.classList.add('red', 'blue');
+
+console.log(boxElement.classList.contains('blue'));
+
+setInterval(() => {
+    boxElement.classList.remove('red');
+}, 3000);
+
