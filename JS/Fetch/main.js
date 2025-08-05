2cb6cfa2b7340fc2bdf99aaea177134b3e584d52
josephhieu <joseph.nguyenhoanghieu@gmail.com>
2025-08-05 21:04:29 +0700

    Promise

--- JS/Fetch/main.js
+++ JS/Fetch/main.js
@@ -21,6 +21,62 @@ console.log(JSON.parse(json));
 console.log(JSON.stringify(true));
 
 // Promise (sync, async)
+// Promise have 3 status
+// 1. Pending
+// 2. Fullfilled
+// 3. Rejected
+
+var promise = new Promise(
+    // Executor
+    function(resolve, reject) {
+
+        // Logic
+        // Success: resolve() - Pending
+        // Faile: reject()
+        // resolve();
+        // reject('Thong bao loi')
+
+    }
+);
+
+promise
+    .then(function() {
+        console.log('Successfully! 1');
+    })
+    .then(function() {
+        console.log('Successfully! 2');
+    })
+    .then(function() {
+        console.log('Successfully! 3');
+    })
+    .catch(function(error) {
+        console.log(error);
+    })
+    .finally(function() {
+        console.log('Done!');
+    });
 
 
+function sleep(ms) {
+    return new Promise(function(resolve) {
+        setTimeout(resolve, ms);
+    })
+}
 
+sleep(1000)
+    .then(function() {
+        console.log(1);
+        return sleep(1000);
+    })
+    .then(function() {
+        console.log(2);
+        return sleep(1000);
+    })
+    .then(function() {
+        console.log(3);
+        return sleep(1000);
+    })
+    .then(function() {
+        console.log(4);
+        return sleep(1000);
+    })
