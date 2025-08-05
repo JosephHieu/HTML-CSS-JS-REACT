8d0516bf6b6154780a6a5f5c40b206a736c76875
josephhieu <joseph.nguyenhoanghieu@gmail.com>
2025-08-05 20:20:48 +0700

    sync asyns

---
+++ JS/Fetch/main.js
@@ -0,0 +1,26 @@
+
+// JSON
+
+// 1. là một định dạng dữ liệu (chuỗi)
+// 2. JavaScript Object Notation
+// 3. JSON: Number, String, Boolean, Null, Array
+// Object
+
+// Mã hóa / giải mã
+// Encode / decode
+// Stringify: Từ Javascript -> JSON
+// Parse: TỪ JSON -> Javascript types
+
+// var json = '["Javascript","PHP"]';
+var json = '{"name":"Son Dang","age":18}';
+
+var a = '1';
+
+console.log(JSON.parse(json));
+
+console.log(JSON.stringify(true));
+
+// Promise (sync, async)
+
+
+
