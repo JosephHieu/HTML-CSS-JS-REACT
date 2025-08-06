ab78be91ae01425e693fd5b1ce56179fdf1148d5
josephhieu <joseph.nguyenhoanghieu@gmail.com>
2025-08-06 20:45:11 +0700

    Promise Example

--- JS/Fetch/main.js
+++ JS/Fetch/main.js
@@ -1,4 +1,3 @@
-
 // JSON
 
 // 1. là một định dạng dữ liệu (chuỗi)
@@ -14,7 +13,7 @@
 // var json = '["Javascript","PHP"]';
 var json = '{"name":"Son Dang","age":18}';
 
-var a = '1';
+var a = "1";
 
 console.log(JSON.parse(json));
 
@@ -27,56 +26,131 @@ console.log(JSON.stringify(true));
 // 3. Rejected
 
 var promise = new Promise(
-    // Executor
-    function(resolve, reject) {
-
-        // Logic
-        // Success: resolve() - Pending
-        // Faile: reject()
-        // resolve();
-        // reject('Thong bao loi')
-
-    }
+  // Executor
+  function (resolve, reject) {
+    // Logic
+    // Success: resolve() - Pending
+    // Faile: reject()
+    // resolve();
+    // reject('Thong bao loi')
+  }
 );
 
 promise
-    .then(function() {
-        console.log('Successfully! 1');
-    })
-    .then(function() {
-        console.log('Successfully! 2');
-    })
-    .then(function() {
-        console.log('Successfully! 3');
-    })
-    .catch(function(error) {
-        console.log(error);
-    })
-    .finally(function() {
-        console.log('Done!');
-    });
-
+  .then(function () {
+    console.log("Successfully! 1");
+  })
+  .then(function () {
+    console.log("Successfully! 2");
+  })
+  .then(function () {
+    console.log("Successfully! 3");
+  })
+  .catch(function (error) {
+    console.log(error);
+  })
+  .finally(function () {
+    console.log("Done!");
+  });
 
 function sleep(ms) {
-    return new Promise(function(resolve) {
-        setTimeout(resolve, ms);
-    })
+  return new Promise(function (resolve) {
+    setTimeout(resolve, ms);
+  });
 }
 
-sleep(1000)
-    .then(function() {
-        console.log(1);
-        return sleep(1000);
-    })
-    .then(function() {
-        console.log(2);
-        return sleep(1000);
-    })
-    .then(function() {
-        console.log(3);
-        return sleep(1000);
-    })
-    .then(function() {
-        console.log(4);
-        return sleep(1000);
-    })
+// sleep(1000)
+//   .then(function () {
+//     console.log(1);
+//     return sleep(1000);
+//   })
+//   .then(function () {
+//     console.log(2);
+//     return sleep(1000);
+//   })
+//   .then(function () {
+//     console.log(3);
+//     return sleep(1000);
+//   })
+//   .then(function () {
+//     console.log(4);
+//     return sleep(1000);
+//   });
+
+// Promise Example
+var users = [
+  {
+    id: 1,
+    name: "Nguyen Hoang Hieu",
+  },
+  {
+    id: 2,
+    name: "Tran Minh Lieu",
+  },
+  {
+    id: 3,
+    name: "Hoang Lang Tu",
+  },
+];
+
+var comments = [
+  {
+    id: 1,
+    user_id: 1,
+    content: "Em an com chua",
+  },
+  {
+    id: 2,
+    user_id: 2,
+    content: "Roi may",
+  },
+];
+
+// Hàm giả lập API lấy comments
+function getComments() {
+  return new Promise(function (resolve) {
+    setTimeout(function () {
+      resolve(comments);
+    }, 1000);
+  });
+}
+
+// Hàm giả lập API lấy user theo ID
+function getUsersByIds(userIds) {
+  return new Promise(function (resolve) {
+    var result = users.filter(function (user) {
+      return userIds.includes(user.id);
+    });
+    setTimeout(function () {
+      resolve(result);
+    }, 1000);
+  });
+}
+
+// Gọi chuỗi promise
+getComments()
+  .then(function (comments) {
+    var userIds = comments.map(function (comment) {
+      return comment.user_id;
+    });
+
+    return getUsersByIds(userIds).then(function (users) {
+      return {
+        users: users,
+        comments: comments,
+      };
+    });
+  })
+  .then(function (data) {
+    var commentBlock = document.getElementById("comments-block");
+
+    var html = "";
+    data.comments.forEach((comment) => {
+      var user = data.users.find(function (user) {
+        return user.id === comment.user_id;
+      });
+      html += `<li>${user.name}: ${comment.content}</li>`;
+    });
+
+    commentBlock.innerHTML = html;
+  });
